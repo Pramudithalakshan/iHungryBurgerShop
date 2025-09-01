65a0311ef9a734f7ded7e0fa2bb4bc0fe1dd354b
Pramudithalakshan <pramudithalakshan00@gmail.com>
2025-09-01 16:27:36 +0530

    Initial commit

---
+++ Example.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+     Scanner input = new Scanner(System.in);
+ 	 System.out.println("------------------------------------------------------");
+ 	 System.out.println("|                 iHungry Burger                     |");
+ 	 System.out.println("------------------------------------------------------");
+ 	 System.out.println();
+     System.out.println("[1] Place Order \t\t[2] Search Best Customer");
+     System.out.println("[3] Search Order \t\t[4] Search Customer");
+     System.out.println("[5] View Orders \t\t[6] Update Order Details");
+     System.out.println("[7] Exit");
+     System.out.println();
+     System.out.print("Enter an option to continue >");
+     int op = input.nextInt();
+		 switch(){
+			 case 1:
+			 //placeOrder();break;
+			 case 2:
+			 //searchBestCustomer();break;
+			 case 3:
+			 //searchOrder();break;
+			 case 4:
+			 //searchCustomer();break;
+			 case 5:
+			 //viewOrders();break;
+			 case 6:
+			 //exit();break;
+			 default:	 
+	}
+	 
+}
