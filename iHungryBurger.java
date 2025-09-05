d6fec689da415880ac31fa9251e37279eb3ef5dd
Pramudithalakshan <pramudithalakshan00@gmail.com>
2025-09-05 07:06:28 +0530

    Added new java file instead Example.java file

--- iHungryBurger.java
+++ iHungryBurger.java
@@ -1,17 +1,21 @@
 import java.util.*;
-class Example{
-	public static int[] cusId=new int[100];
-    public static String[] cusName=new String[100]; 
-    public static String[] OrderIds=new String[100];
-    public static double[] totals=new double[100];  
-    public static int[] tempOrIds={0};
-    public static int count=0;
+class iHungryBurger{
+    //Global arrays
+    public static String[] orderId=new String[0];
+    public static int[] cusId=new int[0];
+    public static String[] cusName=new String[0];
+    public static int[] quantity=new int[0];
+    //CONSTANT
+    public final static double UNIT_PRICE=500;
+    public final static int PREPARING=0;
+    public final static int DELIVERED=1;
+    public final static int CANCEL=2;
+    
 	public static void main(String args[]){
-    mainMenu();     
-}
-//////////////Main Menu////////////////
-public static void mainMenu(){
-	 Scanner input = new Scanner(System.in);
+		 mainMenu(); 
+	}
+	public static void mainMenu(){
+		 Scanner input = new Scanner(System.in);
  	 System.out.println("------------------------------------------------------");
  	 System.out.println("|                 iHungry Burger                     |");
  	 System.out.println("------------------------------------------------------");
@@ -28,7 +32,7 @@ public static void mainMenu(){
 			 clearConsole();
 			 placeOrder();break;
 			 case 2:
-			 searchBestCustomer();break;
+			 //searchBestCustomer();break;
 			 case 3:
 			 //searchOrder();break;
 			 case 4:
@@ -38,11 +42,10 @@ public static void mainMenu(){
 			 case 6:
 			 //exit();break;
 			 default:	 
-	}
-	
-	
-	}
-//////////////Place Order////////////////
+	       }
+		}
+		
+	//////////////Place Order////////////////
 	public static void placeOrder(){
 	
      Scanner input = new Scanner(System.in); 
@@ -50,7 +53,7 @@ public static void mainMenu(){
      System.out.println("------------------------------------------------------");
  	 System.out.println("|                    PLACE ORDER                     |");
  	 System.out.println("------------------------------------------------------");
- 	 String orId = generateOrderID();
+ 	 String orId = generateOrderId();
  	 System.out.println();
  	 System.out.println();
  	 System.out.println("ORDER ID - "+orId);
@@ -60,23 +63,25 @@ public static void mainMenu(){
  	 System.out.println();
  	 L1:while(true){
  	 System.out.print("Enter Customer ID (phone no.): ");
- 	 String phoneNo = input.next();
- 	 if(validatePhoneNo(phoneNo)){
-		        if(isCustomer(count,phoneNo)){
-				  int index =searchIndex(phoneNo);
+ 	 String phone = input.next();
+ 	 
+ 	 if(validatePhoneNo(phone)){                   //validate phone number
+		        int phoneNo = Integer.parseInt(phone);
+		     
+		        if(isCustomer(phoneNo)){
+				             //Search, is customer already added
+				  int index =searchIndex(phoneNo);  //Get a index of customer
 		          System.out.println("Customer Name :"+cusName[index]);
 		          System.out.print("Enter Burger Quantity - :");
 		          int qty = input.nextInt();
-		           double total = qty*500;
+		          double total = qty*UNIT_PRICE;
 		          System.out.println("Total value - "+total);
 		          int decision = confirmOrder();
 		          switch(decision){
 					  case 1:   //Confirm order
-					  OrderIds[count]=orId;
-					  count++;
+					  addOrderId(orId);
 					  System.out.println();
-					 
-					   totals[index]=totals[index]+total;
+					  quantity[index]=quantity[index]+qty;
 					  System.out.println("Your order is entered to the system successfully");
 					   L2:while(true){
 						   int serviceDeci = anotherService();
@@ -84,8 +89,14 @@ public static void mainMenu(){
 					       case 1:
 					       clearConsole();
 					       continue PO;
-					       case 2:
-					       clearConsole();
+					       case 2: 
+					       //check data of the arrays
+					        System.out.println(cusName[0]);
+					        System.out.println(cusId[0]);
+					        System.out.println(quantity[0]);
+					        System.out.println(Arrays.toString(orderId));
+					         System.out.println(Arrays.toString(cusName));                    
+					       //clearConsole();
 					       mainMenu();
 					       return;
 					       case 3:
@@ -111,21 +122,21 @@ public static void mainMenu(){
 					   }
 				    }
 		          }else{
-                    
-					  cusId[count]=Integer.parseInt(phoneNo);
+					  
 					  System.out.print("Customer Name :");
-					  cusName[count]=input.next();
+					  String name=input.next();   //Name of the new customer
 		              System.out.print("Enter Burger Quantity - :");
 		              int qty = input.nextInt();
-		              double total = qty*500;
-		              System.out.println("Total value - "+total);
+		              double total = qty*UNIT_PRICE;
+		              System.out.println("Total value - "+total);  //Total
 		              
  		              int decision = confirmOrder();
 		              switch(decision){
 					  case 1:
-					  OrderIds[count]=orId;
-					  totals[count]=total;
-					  count++;
+					  addOrderId(orId);
+					  addNewCustomerId(phoneNo);  //Add new customer
+					  addNewQuantity(qty);
+					  addNewCustomerName(name);
 					  System.out.println();
 					  
 					  System.out.println("Your order is entered to the system successfully");
@@ -135,7 +146,7 @@ public static void mainMenu(){
 					       case 1:
 					       clearConsole();
 					       continue PO;
-					       case 2:
+					       case 2:    
 					       clearConsole();
 					       mainMenu();
 					       return;
@@ -170,10 +181,28 @@ public static void mainMenu(){
 			 }
  	     }
 	 }
-     
-	}
-//////////////Another Service////////////////
-  public static int anotherService(){
+ }   
+        
+       //////////////Search Index Number////////////////	
+       public static int searchIndex(int phoneNo){
+		 for (int i = 0; i <cusId.length; i++){
+			 if(cusId[i]==phoneNo){
+				 return i;
+				 }
+		 }
+		 return -1;
+		}
+      //////////////Search Customer////////////////
+      public static boolean isCustomer(int phoneNo){
+		for (int i = 0; i <cusId.length; i++){
+			if(cusId[i]==phoneNo){
+			 return true;
+			 }		
+		  }
+		   return false;
+	   }
+      //////////////Another Service////////////////
+      public static int anotherService(){
 		Scanner input = new Scanner(System.in);
 		System.out.print("Do you want to place another order (Y/N) - ");
 		String decision = input.next().toUpperCase();
@@ -186,8 +215,44 @@ public static void mainMenu(){
 			return 3;
 			}
 		} 
-//////////////Confirm order////////////////
-  public static int confirmOrder(){
+     ///////////////Add order id/////////////////
+     public static void addOrderId(String id){
+		  String[] tempArray = new String[orderId.length+1];
+		  for (int i = 0; i < orderId.length; i++){
+			  tempArray[i]=orderId[i];
+		  }
+		  tempArray[tempArray.length-1]=id;
+		  orderId=tempArray;
+		 }
+	  ///////////////Add new customerid/////////////////
+     public static void addNewCustomerId(int id){
+		  int[] tempArray = new int[cusId.length+1];
+		  for (int i = 0; i < cusId.length; i++){
+			  tempArray[i]=cusId[i];
+		  }
+		  tempArray[tempArray.length-1]=id;
+		  cusId=tempArray;
+		 }
+	  ///////////////Add new customer name/////////////////
+     public static void addNewCustomerName(String name){
+		  String[] tempArray = new String[cusName.length+1];
+		  for (int i = 0; i < cusName.length; i++){
+			  tempArray[i]=cusName[i];
+		  }
+		  tempArray[tempArray.length-1]=name;
+		  cusName=tempArray;
+		 }
+	 ///////////////Add qty////////////////
+     public static void addNewQuantity(int qty){
+		  int[] tempArray = new int[quantity.length+1];
+		  for (int i = 0; i < quantity.length; i++){
+			  tempArray[i]=quantity[i];
+		  }
+		  tempArray[tempArray.length-1]=qty;
+		  quantity=tempArray;
+		 }
+     //////////////Confirm order/////////////////
+     public static int confirmOrder(){
 		Scanner input = new Scanner(System.in);
 		System.out.print("\tAre you confirm order - ");
 		String decision = input.next().toUpperCase();
@@ -199,66 +264,23 @@ public static void mainMenu(){
 			default :
 			return 3;
 			}
-		} 
-//////////////Search Index Number////////////////	
-    public static int searchIndex(String phoneNo){
-	    	int ph=Integer.parseInt(phoneNo);
-		 for (int i = 0; i <cusId.length ; i++){
-			 if(cusId[i]==ph){
-				 return i;
-				 }
-		 }
-		 return -1;
 		}
-//////////////Validate Phone Number////////////////	
+	//////////////Generate order id////////////////		
+	public static String generateOrderId(){
+		if(orderId.length==0){
+			return "B0001";
+		}
+		String ls=orderId[orderId.length-1].substring(1);
+		return String.format("B%04d",Integer.parseInt(ls)+1);
+	}
+	
+	//////////////Validate Phone Number////////////////	
     public static boolean validatePhoneNo(String phoneNo){
 		 int ch = phoneNo.charAt(0); 	 
 		 return ch!=48||phoneNo.length()!=10?false:true;
 		}
-//////////////Search Customer////////////////
-    public static boolean isCustomer(int count,String phoneNo){
-		int ph=Integer.parseInt(phoneNo);
-		for (int i = 0; i <=count; i++){
-			if(cusId[i]==ph){
-			 return true;
-			 }		
-		  }
-		   return false;
-	}
-//////////////Generate orderId////////////////
-    public static String generateOrderID(){
- 	 tempOrIds[0]=tempOrIds[0]+1;
- 	 String[] orIds=new String[1];
- 	 orIds[0]=""+tempOrIds[0];// 
- 	 while(orIds[0].length()<4){
-		 orIds[0]="0"+ orIds[0];
-		 }
-	 orIds[0]="B"+ orIds[0];
-	return orIds[0];
-	}
 	
-	
-	
-	public static void searchBestCustomer(){
-		 
-		 for (int i = 0; i < count; i++){
-			 int phoneNo=cusId[i];
-			 int index = searchIndex(phoneNo+"");
-			 System.out.println(cusName[index]);
-		 }
-		 
-		for (int i = 0; i < count; i++){
-			System.out.println(totals[i]);
-		}
-		 
-		}
-	
-	
-	
-	
-	
-	
-//////////////Clear Console////////////////
+	//////////////Clear Console////////////////
 	public final static void clearConsole() {
      try {
       final String os =
@@ -273,6 +295,5 @@ public static void mainMenu(){
        e.printStackTrace();
        // Handle any exceptions.
      }
-  }
-	 
+   }
 }
